3a472e6723eeec29983612e76bf386fc1e0b8f99
Alexandru-Valentin Harapu <aharapu2@yahoo.com>
2020-06-25 16:38:47 +0300

    basic modal

--- ui/src/components/blogComponents/Title.js
+++ ui/src/components/blogComponents/Title.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Button, ButtonGroup, Card, Modal, Slide, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -10,26 +10,82 @@ const useStyles = makeStyles((theme) => ({
 		},
 		[theme.breakpoints.up('md')]: {
 			paddingTop: '40px',
-			paddingBottom: '30px',
 		},
 		[theme.breakpoints.up('xl')]: {
 			paddingTop: '60px',
+		},
+		textShadow: '0.5px 0.5px 0px #889b43',
+		marginBottom: '10px',
+	},
+	descriptionCard: {
+		[theme.breakpoints.up('xs')]: {
+			paddingTop: '20px',
+			paddingBottom: '20px',
+		},
+		[theme.breakpoints.up('md')]: {
+			paddingBottom: '30px',
+		},
+		[theme.breakpoints.up('xl')]: {
 			paddingBottom: '50px',
 		},
-		textShadow: '1px 1px 4px #889b43',
+	},
+	modalCard: {
+		position: 'absolute',
+		top: '50%',
+		transform: 'translateY(-50%)',
+		left: '50%',
+		transform: 'translateX(-50%)',
+		'&:focus': {
+			outline: 'none',
+		},
+	},
+	removeFocus: {
+		'&:focus': {
+			outline: 'none',
+		},
 	},
 }));
 
 export default function Title() {
 	const classes = useStyles();
+	const description =
+		'while (awake) { if (hungry) then { function findDeliciousFood(){ if (happy) then foundIt = true } if (foundIt == true) return blogPost; }';
+
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+
+	function closeModal() {
+		setModalIsOpen(false);
+	}
+
 	return (
-		<Card variant='elevation' elevation={10} className={classes.titleCard}>
+		<>
+			<Card variant='elevation' elevation={10} className={classes.titleCard}>
 				<Typography variant='h3' color='initial' align='center' gutterBottom={true}>
 					Hipster Foodie Beta
 				</Typography>
 				<Typography variant='subtitle2' color='initial'>
 					Half webDev, half chef, 100% geek. You're welcome!
 				</Typography>
-		</Card>
+			</Card>
+			<Card variant='elevation' elevation={10} className={classes.descriptionCard}>
+				<Typography variant='subtitle2' align='justify' gutterBottom='true'>
+					{description}
+				</Typography>
+				<ButtonGroup color='primary' aria-label='outlined primary button group'>
+					<Button onClick={() => setModalIsOpen(!modalIsOpen)}>{'<About />'}</Button>
+					<Button onClick={() => setModalIsOpen(!modalIsOpen)}>
+						{'<SocialMedia />'}
+					</Button>
+					<Button onClick={() => setModalIsOpen(!modalIsOpen)}>
+						{'<RandomArticle />'}
+					</Button>
+				</ButtonGroup>
+			</Card>
+			<Modal open={modalIsOpen} onClose={() => setModalIsOpen(!modalIsOpen)}>
+				<Card variant='elevation' elevation={10} className={classes.modalCard}>
+					some modal content
+				</Card>
+			</Modal>
+		</>
 	);
 }
